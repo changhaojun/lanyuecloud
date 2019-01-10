7f05c30a44dbfd0418250f538a414a82e1602edd
changhaojun648859757@qq.com <changhaojun648859757@qq.com>
2019-01-10 15:27:00 +0800

    临时提交

--- js/head.js
+++ js/head.js
@@ -1,12 +1,15 @@
 (function() {
     const Head = {
         init: function() {
-            const menuProductTitleList = [].slice.call($('.block-1 .title-ul>li'));
-            const menuProductContentList = [].slice.call($('.block-1 .content-ul>li'));
-            const menuCaseTitleList = [].slice.call($('.block-2 .title-ul>li'));
-            const menuCaseContentList = [].slice.call($('.block-2 .content-ul>li'));
-            tabSwitch(menuProductTitleList, menuProductContentList);
-            tabSwitch(menuCaseTitleList, menuCaseContentList);
+            // const menuProductTitleList = [].slice.call($('.block-1 .title-ul>li'));
+            // const menuProductContentList = [].slice.call($('.block-1 .content-ul>li'));
+            // const menuCaseTitleList = [].slice.call($('.block-2 .title-ul>li'));
+            // const menuCaseContentList = [].slice.call($('.block-2 .content-ul>li'));
+            // tabSwitch(menuProductTitleList, menuProductContentList);
+            // tabSwitch(menuCaseTitleList, menuCaseContentList); 
+
+            
+            
         }
     }
     Head.init();
