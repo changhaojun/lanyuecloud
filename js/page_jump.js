83a93f37e1a2aa98b1e40e59fd50caa5346a0c5e
changhaojun648859757@qq.com <changhaojun648859757@qq.com>
2019-01-11 13:36:13 +0800

    添加悬浮图标

--- js/page_jump.js
+++ js/page_jump.js
@@ -2,8 +2,6 @@ function pageJump(event) {
     const element = event.target;
     let href = element.getAttribute('data-href');
     let currentIndex =  element.getAttribute('data-i');
-    console.log(href);
-    console.log(currentIndex);
     window.sessionStorage.setItem('href', href);
     window.sessionStorage.setItem('currentIndex', currentIndex);
     if(href == 'home.html') {
